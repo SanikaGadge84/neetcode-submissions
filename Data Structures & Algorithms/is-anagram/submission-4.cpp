f18431308648576e9ecd147cdc2137832d18fd2f
SanikaGadge84 <sanikagadge84@gmail.com>
2026-06-25 11:46:49 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        unordered_map<char,int>Scount;
+        unordered_map<char,int>Tcount;
+        for(int i=0;i<s.length();i++){
+            Scount[s[i]]++;
+            Tcount[t[i]]++;
+        }
+        if(Scount==Tcount){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
