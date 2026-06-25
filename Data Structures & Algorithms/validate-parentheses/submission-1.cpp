9deddde6e4cc6f1ed618c6f28f56fd042ef986d1
SanikaGadge84 <sanikagadge84@gmail.com>
2026-06-25 11:34:42 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        std::stack<char>stack;
+        std::unordered_map<char,char>mp={
+            {')','('},
+            {']','['},
+            {'}','{'}
+        };
+        for(char c:s){
+            if(mp.count(c)){
+                if(!stack.empty() && stack.top()==mp[c]){
+                    stack.pop();
+                }else{
+                    return false;
+                }
+            }
+            else{
+                stack.push(c);
+            }
+        } return stack.empty();
+    }
+};
